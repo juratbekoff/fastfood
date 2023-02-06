9aea23ba157603c9fa2a4e1e07b1fc4125d86295
unknown <juratbekxudayberganov9@gmail.com>
2023-02-06 19:17:18 +0500

    index folder is removed

--- src/imports/register.import.ts
+++ src/imports/register.import.ts
@@ -1,6 +1,6 @@
 import { UserRegisterDto } from "../types";
 import { v4 as uuid } from "uuid"
-import mailSender from "./../controller/Auth/mailSender";
+import mailSender from "../controller/auth/mailSender";
 import { Role } from "@prisma/client";
 
 export {
